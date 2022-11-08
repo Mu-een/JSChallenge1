9976126f1451f794703a143c3119db441111e41a
Mu-een <mueenslam@gmail.com>
2022-11-08 11:06:59 +0200

    JS Challenge

---
+++ js/code.js
@@ -0,0 +1,11 @@
+let firstName = prompt("Enter Name")
+let mySurname = prompt("Enter Surname")
+let yourSalary = prompt("Enter Salary")
+let bonus = 500
+let product = eval(`${yourSalary}+${bonus}`);
+
+document.write("Name:" + firstName)
+document.write("<br>Surname:" + mySurname)
+document.write("<br>Salary:R" + product)
+
+
